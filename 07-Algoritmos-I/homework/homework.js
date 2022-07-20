f16543df4cccad73dcf42f05688b08ae9fce91c0
manuelturlan <manuelturlan18@gmail.com>
2022-07-20 14:04:01 -0400

    homework 07

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function factorear(num) {
@@ -6,7 +6,17 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let array = [1];
+  let i = 2;
+  while (num > 1) {
+    if (num % i === 0) {
+      array.push(i);
+      num = num / i;
+    } else {
+      i++;
+    }
+  }
+  return array;
 }
 
 function bubbleSort(array) {
@@ -14,27 +24,46 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        let aux = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = aux;
+      }
+    }
+  }
+  return array;
 }
-
 
 function insertionSort(array) {
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      let aux = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = aux;
+    }
+    while (array[i] < array[i - 1]) {
+      let aux = array[i];
+      array[i] = array[i - 1];
+      array[i - 1] = aux;
+      i--;
+    }
+  }
 
+  return array;
 }
-
 
 function selectionSort(array) {
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
